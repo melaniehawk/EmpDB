53db6dcca58c4abe44a6ad42758c70b6d91e3697
melaniehawk <maraoz@uw.edu>
2025-03-15 11:12:27 -0700

    Update BasePlusCommissionEmployee.cs

--- BasePlusCommissionEmployee.cs
+++ BasePlusCommissionEmployee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,35 @@ namespace EmpDB
 {
     public class BasePlusCommissionEmployee : CommissionEmployee
     {
+        private decimal baseSalary; // base salary per week
+
+        // six-parameter constructor
+        public BasePlusCommissionEmployee(string firstName, string lastName, string socialSecurityNumber,
+            decimal grossSales, decimal commissionRate, decimal baseSalary)
+            : base(firstName, lastName, socialSecurityNumber, grossSales, commissionRate)
+        {
+            BaseSalary = baseSalary; // validate base salary
+        }
+
+        // property that gets and sets BasePlusCommissionEmployee's base salary
+        public decimal BaseSalary
+        {
+            get 
+            {
+                return baseSalary; 
+            }
+            set
+            {
+                if (value < 0) // validation
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(BaseSalary)} must be >= 0");
+                }
+
+                baseSalary = value;
+            }
+        }
+        // calculate earnings
+        public override decimal Earnings() => BaseSalary + base.Earnings(); // base class earnings + base salary
 
     }
 }
